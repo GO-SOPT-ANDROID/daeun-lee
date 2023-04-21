3b44c20470b29624b8ee3ccf99fd9422e0216774
Dan2dani <eldms0406@naver.com>
2023-04-21 21:54:06 +0900

    [feat] 바텀 네비게이션 구현

---
+++ app/src/main/java/org/android/go/sopt/presentation/search/SearchFragment.kt
@@ -0,0 +1,32 @@
+package org.android.go.sopt.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.android.go.sopt.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding
+        get() = requireNotNull(_binding!!) { "${this::class.java.simpleName}에서 에러가 발생했습니다." }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
