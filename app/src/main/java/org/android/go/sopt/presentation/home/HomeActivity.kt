f800ad6119840d8e31447fa5e61fa546332a5546
Dan2dani <eldms0406@naver.com>
2023-04-21 21:57:23 +0900

    [feat] 바텀 네비게이션 로직 구현(선택한 버튼에 따라 fragment 바뀌도록)

--- app/src/main/java/org/android/go/sopt/presentation/home/HomeActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/HomeActivity.kt
@@ -1,21 +1,39 @@
 package org.android.go.sopt.presentation.home
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import org.android.go.sopt.R
 import org.android.go.sopt.databinding.ActivityHomeBinding
-import org.android.go.sopt.presentation.common.ViewModelFactory
+import org.android.go.sopt.presentation.gallery.GalleryFragment
+import org.android.go.sopt.presentation.search.SearchFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
+        changeFragment(HomeFragment())
+
+        binding.bnvHome.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menu_home -> changeFragment(HomeFragment())
+                    R.id.menu_gallery -> changeFragment(GalleryFragment())
+                    R.id.menu_search -> changeFragment(SearchFragment())
+                }
+                true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.home_container, fragment)
+            .commit()
     }
 }
