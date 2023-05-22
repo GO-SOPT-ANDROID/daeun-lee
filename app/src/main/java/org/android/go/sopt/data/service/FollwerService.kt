982a230b83356ac0c90828607130f7dec304bef6
Dan2dani <eldms0406@naver.com>
2023-05-22 15:01:13 +0900

    [mod] FollowerService Query문으로 수정

--- app/src/main/java/org/android/go/sopt/data/service/FollwerService.kt
+++ app/src/main/java/org/android/go/sopt/data/service/FollwerService.kt
@@ -2,8 +2,11 @@ package org.android.go.sopt.data.service
 
 import org.android.go.sopt.data.model.response.ResponseFollower
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FollwerService {
-    @GET("api/users?page=2")
-    suspend fun fetchFollowerList(): ResponseFollower
+    @GET("api/users")
+    suspend fun fetchFollowerList(
+        @Query("page") page: Int = 2,
+    ): ResponseFollower
 }
